ea6b4648f1b1ab8b05671cb8d715efdfe2e66cb6
AlineGuiseline <alineguiseline@gmail.com>
2023-08-07 15:52:47 -0300

    creates react project

---
+++ src/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import Background1 from './assets/background1.svg';
+
+const Container = styled.div `
+    background: url('${Background1}');
+    background-size: cover;
+`
+
+const Image = styled.img `
+    
+`
+
+const ContainerItems = styled.div `
+
+`
+
+const H1 = styled.h1 `
+
+`
+
+const InputLabel = styled.p `
+
+`
+
+const Input = styled.input `
+
+`
+
+const Button = styled.button `
+
+`
+
+export {Container, Image, ContainerItems, H1, InputLabel, Input, Button}
